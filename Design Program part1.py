89886bd01fdc218d0aac442eaa2c8a001b30da9e
ArronBurris <46752697+ArronBurris@users.noreply.github.com>
2019-01-28 13:48:57 -0600

    Fuck

---
+++ Design Program part1.py
@@ -0,0 +1,18 @@
+Problem:Calculate cost of video games for christmas presents
+Requirements:
+    Cost of video games
+    Input:Number of relatives that want videos games
+    Input:Number of video games per relative
+    Output:Total number of video games
+    Output:Total cost of all video games
+
+
+Declare Costpervideogame = 60
+Declare Numberofrelatives = 0
+Declare Videogamesperrelative = 0
+Declare Numberofvideogames = 0
+    input=Numberofrelatives
+    input=Videogamesperrelative
+Declare Numberofvideogames = Numberofrelatives * Videogamesperrelative
+Totalcost = Numberofvideogames * Costpervideogame
+print (Totalcost)
