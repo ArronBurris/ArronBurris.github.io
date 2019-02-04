c3a8625b07f419c4ef09aa12c63b61858f19639d
ArronBurris <46752697+ArronBurris@users.noreply.github.com>
2019-02-04 13:44:59 -0600

    booty

---
+++ In class tests/Python Practice and Notes file.py
@@ -0,0 +1,16 @@
+print("test\n new line")
+print("test2")
+print("This is the first line\n This is the second line but also indented")
+print("this is my \"quote\"")
+print("this is my \'pussy\'")
+print("this is my slash\\")
+print("https://otc.instructure.com/courses/17493/modules")
+print("na " * 8 + "batman")
+print("Im gonna say the N-word \
+      You can't stop me you fuck \
+      I'm gonna do it \
+      Niqueragua")
+words = "This is mY ASs"
+print(words.upper())
+print(words.lower())
+print(words.swapcase())
---
+++ Video game Python.py
@@ -0,0 +1,13 @@
+Costpervideogame = 60
+Numberofrelatives = 0
+Videogamesperrelative = 0
+Numberofvideogames = 0
+totalPrice = 0
+
+Numberofrelatives = int(input("Ey man... how many of yo fellas you lookin' to get games for? "))
+Videogamesperrelative = int(input("Aight cool cool... but how many games you lookin' to get for each of 'em?" ))
+
+Numberofvideogames = Numberofrelatives * Videogamesperrelative
+
+totalPrice = Numberofvideogames * Costpervideogame
+print("Yo what you're doin is awesome man they're gonna love these games. Total damage is $ " + str(totalPrice))
