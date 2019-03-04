f06514081d36ef42c1d94c7b7c5bc521cbb8662e
ArronBurris <46752697+ArronBurris@users.noreply.github.com>
2019-03-04 00:07:03 -0600

    dftg

---
+++ Reverse thing.py
@@ -0,0 +1,11 @@
+UserMessage = 0
+
+print("Ey yo give say something short, like 20 character long kinda short.")
+print("You do that and i'll reverse it for your... nothing dirty though.")
+
+input(str(UserMessage))
+
+UserMessage = UserMessage[::-1]
+
+print(str(UserMessage))
+    
