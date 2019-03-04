1c29909540766facbf548b886478ff7ee49198e0
ArronBurris <46752697+ArronBurris@users.noreply.github.com>
2019-03-04 09:47:33 -0600

    askjdf

---
+++ In class tests/Loops stuff.py
@@ -0,0 +1,22 @@
+#loops
+
+for i in range(8):
+    potato = i + 1
+
+   
+
+    """
+    if (potato == 1 or potato == 2 or potato == 3 or potato == 4 or potato == 5 or potato = 6 or potato == 7):
+        print(str(potato) + " potato")
+    elif (potato == 4):
+        print("Four")
+    elif (potato == 8):
+        print("more")"""
+
+
+    if (potato == 4):
+        print("Four")
+    elif (potato == 8):
+        print("more")
+    else:
+        print(str(potato) + " potato")
