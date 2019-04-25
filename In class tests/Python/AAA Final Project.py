ac856dcb1f814c90a426321b3f71e664b56bae66
ArronBurris <46752697+ArronBurris@users.noreply.github.com>
2019-04-24 22:55:59 -0500

    jhtgjhghj

--- In class tests/Python/AAA Final Project.py
+++ In class tests/Python/AAA Final Project.py
@@ -4,21 +4,47 @@ import tkinter as tk
 
 box = tk.Tk()
 box.title("Final Project")
-box.geometry("1000x1000")
+box.geometry("500x500")
 box.resizable(0,0)
 
-welcomelabel = tk.Label(box,text="Welcome")
+welcomelabel = tk.Label(box, text="Welcome to this shitty GUI")
 welcomelabel.pack()
 
 
-print("Hey what car u want")
+
+
+def Jeep_Choice():
+    Cartype = "Jeep"
+    JeepChoiceLabel = tk.Label(box, text="You chose a Jeep!")
+    JeepChoiceLabel.pack()
+
+def Suv_Choice():
+    Cartype = "SUV"
+    print("You chose an SUV!")
+
+OptionSuv = tk.Button(box, text='SUV', width=25, command=Suv_Choice)
+
+OptionJeep = tk.Button(box, text='Jeep', width=25, command=Jeep_Choice)
 
 def Hey():
+    Cartype = False
+    Cartypelabel = tk.Label(box, text="Which type of car would you like?")
+    Cartypelabel.pack()
+    OptionJeep.pack()
+    OptionSuv.pack()
+        
     
 
-
+def Main():
+    #Put while loop here
+    Cartype = False
+    question = input("Do you want to buy a car?")
+    if question == "Yes":
+        Hey()
         
 
-Option1 = tk.Button(box, text='Option1', width=25, command=Hey)
+Main()
 
-Option1.pack()
+
+
+
